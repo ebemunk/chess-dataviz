3aebe91b3695c28d6926ea5147a926b87e0fe372
ebemunk <ebemunk@gmail.com>
2015-11-05 05:23:00 -0800

    retire old stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,5 +1,5 @@
 /*eslint no-console: 0*/
-/*eslint no-unused-vars: 0*/
+/*eslint no-unused-lets: 0*/
 
 'use strict';
 
@@ -9,180 +9,57 @@ let Promise = require('bluebird');
 let fs = Promise.promisifyAll(require('fs'));
 let Chess = require('chess.js').Chess;
 let _ = require('lodash');
+let highland = require('highland');
 
 let minimist = require('minimist');
 let colors = require('colors');
-let pace = require('pace');
 
 let argv = minimist(process.argv.slice(2), {
 	alias: {
 		file: 'f'
 	},
 	default: {
-		file: 'data/games.pgn'
+		file: 'data/new.pgn'
 	}
 });
 
-debug('  source file'.cyan, argv.file);
+let Openings = require('./lib/Openings.js');
+let Heatmaps = require('./lib/Heatmaps.js');
 
-Promise.coroutine(processPGNFile)(argv.file)
-.spread(function (heatmaps, openings) {
-	return fs.writeFileAsync(argv.file.split('.pgn')[0] + '_statistics.json', JSON.stringify({
+console.log('  source file'.cyan, argv.file);
+
+let heatmaps = new Heatmaps();
+let openings = new Openings();
+
+let start = new Date();
+
+highland(fs.createReadStream(argv.file, {encoding: 'utf8'}))
+.splitBy(/\r?\n\r?\n(?=\[)/g) //split pgns with multiple games by game
+.map(file => {
+	//regexes stolen from chess.js
+	let moves = file
+	.replace(file.replace(/^(\[(.|\r?\n)*\])(\r?\n)*1.(\r?\n|.)*$/g, '$1'), '') //strip away header text
+	.replace(/\r?\n/g, ' ') //join multiple lines
+	.replace(/(\{[^}]+\})+?/g, '') //remove comments
+	.replace(/\d+\./g, '') //remove move numbers
+	.replace(/\.\.\./g, '') //remove ...
+	.replace(/(1\-0)?(0\-1)?(1\/2\-1\/2)?/g, '') //remove results
+	.trim()
+	.split(/\s+/) //split by space
+	//get rid of empty moves
+	.join(',')
+	.replace(/,,+/g, ',')
+	.split(',');
+
+	heatmaps.update(moves);
+	openings.update(_.take(moves, 10));
+})
+.done(() => {
+	fs.writeFileAsync(argv.file.split('.pgn')[0] + '_statistics.json', JSON.stringify({
 		heatmaps: heatmaps.data,
 		openings: openings.data
-	}, null, 4));
-})
-.then(function () {
-	console.log('  all good'.green);
-});
-
-function* processPGNFile(filename) {
-	let file = yield fs.readFileAsync(filename, {encoding: 'utf8'});
-
-	let games = file.split(/\r?\n\r?\n(?=\[)/g);
-	let bar = pace(games.length);
-	let chessGame = new Chess();
-	var heatmaps = new Heatmaps();
-	var openings = new Openings();
-
-	games.map(part => {
-		chessGame.reset();
-		chessGame.load_pgn(part);
-		let moves = chessGame.history({verbose: true});
-
-		heatmaps.update(moves);
-		openings.update(_.take(moves, 10));
-
-		bar.op();
+	}, null, 4))
+	.then(() => {
+		console.log('  done, took'.cyan, new Date().getTime() - start.getTime(), 'ms'.cyan);
 	});
-
-	return [heatmaps, openings];
-}
-
-class Openings {
-	constructor() {
-		this.data = {
-			san: 'start',
-			children: []
-		};
-	}
-
-	update(moves) {
-		let ref = this.data.children;
-
-		moves.forEach(move => {
-			var child = _.find(ref, {san: move.san});
-			if( child ) {
-				child.count++;
-			} else {
-				child = {
-					san: move.san,
-					count: 1,
-					children: []
-				};
-
-				ref.push(child);
-			}
-
-			ref = child.children;
-		});
-	}
-}
-
-class Heatmaps {
-	constructor() {
-		this.counters = [
-			'squareUtilization',
-			'moveSquares',
-			'captureSquares',
-			'checkSquares'
-		];
-
-		this.data = {};
-
-		this.counters.forEach(counter => {
-			this.data[counter] = [];
-
-			for( let i of _.range(64) ) {
-				this.data[counter][i] = {
-					p: {w: 0, b: 0},
-					n: {w: 0, b: 0},
-					b: {w: 0, b: 0},
-					r: {w: 0, b: 0},
-					q: {w: 0, b: 0},
-					k: {w: 0, b: 0},
-					all: {w: 0, b: 0}
-				};
-			}
-		});
-	}
-
-	update(moves) {
-		moves.forEach(move => {
-			move.toIndex = this.squareToIndex(move.to);
-			move.fromIndex = this.squareToIndex(move.from);
-
-			this.counters.forEach(counter => {
-				this[counter](move);
-			});
-		});
-	}
-
-	squareToIndex(square) {
-		square = square.toLowerCase();
-
-		let file = square.charCodeAt(0) - 97;
-		let rank = 8 - square[1];
-		let index = rank * 8 + file;
-
-		return index;
-	}
-
-	squareUtilization(move) {
-		this.data.squareUtilization[move.toIndex][move.piece][move.color]++;
-		this.data.squareUtilization[move.toIndex].all[move.color]++;
-	}
-
-	moveSquares(move) {
-		this.data.moveSquares[move.fromIndex][move.piece][move.color]++;
-		this.data.moveSquares[move.fromIndex].all[move.color]++;
-	}
-
-	captureSquares(move) {
-		if( /c/.test(move.flags) || /e/.test(move.flags) ) {
-			this.data.captureSquares[move.toIndex][move.piece][move.color]++;
-			this.data.captureSquares[move.toIndex].all[move.color]++;
-		}
-	}
-
-	checkSquares(move) {
-		if( /\+$/.test(move.san) ) {
-			this.data.checkSquares[move.toIndex][move.piece][move.color]++;
-			this.data.checkSquares[move.toIndex].all[move.color]++;
-		}
-	}
-
-	// function squareOccupancy (game, boardHeatmap) {
-	// 	var chessGame = new chess.Chess();
-	// 	var files = 'abcdefgh';
-
-	// 	game.notation.forEach(function (move) {
-	// 		var theMove = chessGame.move(move.move);
-	// 		for( let file = 0; file < 8; file++ ) {
-	// 			for( let rank = 1; rank < 9; rank++ ) {
-	// 				let squareName = files.charAt(file) + rank;
-
-	// 				if( theMove.from === squareName ) continue;
-
-	// 				let square = chessGame.get(squareName);
-
-	// 				if( ! square ) continue;
-
-	// 				var squareIndex = squareToIndex(squareName);
-	// 				boardHeatmap[squareIndex[1] * 8 + squareIndex[0]][square.type][square.color]++;
-	// 				boardHeatmap[squareIndex[1] * 8 + squareIndex[0]].all[square.color]++;
-	// 			}
-	// 		}
-	// 	});
-	// }
-}
+});
