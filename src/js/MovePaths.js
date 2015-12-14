669a0428433fa6a94e77ef57835be105ef42468a
ebemunk <ebemunk@gmail.com>
2015-12-14 00:05:34 -0800

    custom bezier curve experiment

--- src/js/MovePaths.js
+++ src/js/MovePaths.js
@@ -2,9 +2,10 @@
 /*eslint no-unused-vars: 0*/
 
 import _ from 'lodash';
-import dbg from 'debug';
+import debug from 'debug';
+import * as util from './util';
 
-let debug = dbg('cdv:MovePaths');
+let log = debug('cv:MovePaths');
 
 export class MovePaths {
 	constructor(selector, options, data) {
@@ -12,63 +13,49 @@ export class MovePaths {
 
 		if( ! selector ) throw Error('need dom selector');
 
-		this.data = data;
-
 		//container setup
-		this.selector = selector;
-
 		this.container = d3.select(selector);
 
 		//options
 		let defaultOptions = {
-			sideLength: 500,
+			width: 500,
 			margin: 20,
-			interactive: true
+			interactive: true,
+			accessor: {
+				piece: 'all',
+				color: 'w'
+			}
 		};
 
 		options = options || {};
+		this._options = _.merge({}, defaultOptions, options);
 
-		this.options = _.merge({}, defaultOptions, options);
-
-		//calculate some things to use later
-		this.options.totalSideLength = this.options.sideLength;
-		this.options.sideLength = this.options.sideLength - this.options.margin * 2;
-		this.options.squareLength = Math.floor(this.options.sideLength / 8);
+		this._options.boardWidth = this._options.width - this._options.margin * 2;
+		this._options.squareWidth = Math.floor(this._options.boardWidth / 8);
 
 		//scale
 		this.scale = d3.scale.linear()
-			.range([0, this.options.squareLength])
+			.range([0, this._options.squareWidth])
 		;
-
-		//board squares
-		let board = [];
-		for( let i = 0; i < 64; i++ ) {
-			board.push({
-				x: i % 8,
-				y: Math.floor(i / 8)
-			});
-		}
-
-		//check if a square is white
-		function isWhite(d) {
-			return (! (d.x % 2) && ! (d.y % 2)) || (d.x % 2 && d.y % 2);
-		}
 
 		//clear element
 		this.container.selectAll('*').remove();
 
 		//root svg
 		let root = this.container.append('svg')
-			.attr('width', this.options.totalSideLength + 'px')
-			.attr('height', this.options.totalSideLength + 'px')
+			.attr('width', this._options.width + 'px')
+			.attr('height', this._options.width + 'px')
 			.attr('class', 'graph')
 		;
 
 		//margins applied
 		let svg = root.append('g')
-			.attr('transform', 'translate(' + this.options.margin + ',' + this.options.margin + ')')
+			.attr('transform', 'translate(' + this._options.margin + ',' + this._options.margin + ')')
 			.attr('class', 'board')
 		;
+
+		//board squares
+		let board = util.boardSquares();
 
 		//create the g elements for squares
 		let squares = svg.selectAll('.square').data(board).enter()
@@ -79,42 +66,42 @@ export class MovePaths {
 
 					return 'square ' + file + rank;
 				})
-				.classed('white', (d) => isWhite(d))
-				.classed('black', (d) => ! isWhite(d))
+				.classed('white', (d) => util.isWhite(d))
+				.classed('black', (d) => ! util.isWhite(d))
 		;
 
 		//create square elements for board squares
 		squares.append('rect')
-			.attr('x', (d) => d.x * this.options.squareLength)
-			.attr('y', (d) => d.y * this.options.squareLength)
-			.attr('width', this.options.squareLength + 'px')
-			.attr('height', this.options.squareLength + 'px')
+			.attr('x', (d) => d.x * this._options.squareWidth)
+			.attr('y', (d) => d.y * this._options.squareWidth)
+			.attr('width', this._options.squareWidth + 'px')
+			.attr('height', this._options.squareWidth + 'px')
+			.attr('class', 'sq')
 		;
 
-		//a-h labels for files
-		let files = d3.range(8).map((i) => String.fromCharCode(97 + i));
 		//labels among the A file
 		let fileLabels = d3.range(8).map((i) => '.a' + (i + 1));
 
 		//file labels
 		svg.selectAll(fileLabels)
 			.append('text')
-				.attr('x', (d) => d.x * this.options.squareLength)
-				.attr('y', (d) => d.y * this.options.squareLength)
+				.attr('x', (d) => d.x * this._options.squareWidth)
+				.attr('y', (d) => d.y * this._options.squareWidth)
 				.attr('dx', '0.2em')
 				.attr('dy', '1em')
 				.text((d) => 8 - d.y)
 				.attr('class', 'label')
 		;
 
-		//labels for 1st rank
+		//a-h labels for files
+		let files = d3.range(8).map((i) => String.fromCharCode(97 + i));
 		let rankLabels = files.slice().map((file) => '.' + file + '1');
 
 		//rank labels
 		svg.selectAll(rankLabels)
 			.append('text')
-				.attr('x', (d) => (d.x + 1) * this.options.squareLength)
-				.attr('y', (d) => (d.y + 1) * this.options.squareLength)
+				.attr('x', (d) => (d.x + 1) * this._options.squareWidth)
+				.attr('y', (d) => (d.y + 1) * this._options.squareWidth)
 				.attr('dx', '-0.3em')
 				.attr('dy', '-0.5em')
 				.attr('text-anchor', 'end')
@@ -128,19 +115,156 @@ export class MovePaths {
 		;
 
 		//tooltip element
-		this.tooltip = this.container
-			.append('div')
-				.attr('class', 'tooltip')
+		this.tooltip = this.container.append('div')
+			.attr('class', 'tooltip')
 		;
 
-		// var d1 = d3.svg.diagonal()
-  //          .source( {"x":0, "y":0} )
-  //          .target( {"x":250, "y":250} );
+		this._data = _.pairs(data.Bc1);
 
-		// this.dataContainer.append("path")
-		//     .attr("stroke", "blue")
-		//     .attr("fill", "transparent")
-		//     .attr("stroke-width", "10")
-		//     .attr("d", d1);
+		let kk = [];
+
+		this._data.forEach(d => {
+			let div = d[1] / 3;
+
+			for(let i=0; i<div; i++)
+				kk.push([d[0], 1]);
+		});
+
+		this.scale = d3.scale.log()
+			.range([0.1, 1])
+		;
+
+		this.scale.domain(
+			d3.extent(this._data, (d) => {
+				return d[1];
+			})
+		);
+
+		this.scale1 = d3.scale.linear()
+			.range([1, 20])
+		;
+
+		this.scale1.domain(
+			d3.extent(this._data, (d) => {
+				return d[1];
+			})
+		);
+
+		var d1 = d3.svg.diagonal()
+			.source((d) => {
+				let square = d[0].split('-')[0].toLowerCase();
+
+				let file = square.charCodeAt(0) - 97;
+				let rank = 8 - square[1];
+
+
+				let x = (file * this._options.squareWidth) + (this._options.squareWidth / 2);
+				let y = (rank * this._options.squareWidth) + (this._options.squareWidth / 2);
+
+				x += _.random(-7, 7, true);
+				y += _.random(-7, 7, true);
+				// log(square)
+				// log(file, rank);
+				// log(x, y);
+				// log('-------------')
+				return {x: x, y: y};
+			})
+			.target((d) => {
+				let square = d[0].split('-')[1].toLowerCase();
+
+				let file = square.charCodeAt(0) - 97;
+				let rank = 8 - square[1];
+
+
+				let x = (file * this._options.squareWidth) + (this._options.squareWidth / 2);
+				let y = (rank * this._options.squareWidth) + (this._options.squareWidth / 2);
+
+				x += _.random(-7, 7, true);
+				y += _.random(-7, 7, true);
+				// log(square)
+				// log(file, rank);
+				// log(x, y);
+				// log('-------------')
+				return {x: x, y: y};
+			})
+
+			;
+
+		this.dataContainer.selectAll('.faf').data(kk)
+		.enter().append("path")
+			// .attr("stroke", "red")
+			// .attr("fill", "transparent")
+			// .attr('stroke-width', '1')
+			// .attr('stroke-opacity', 0.1)
+			// .attr("stroke-width", d => this.scale1(d[1]))
+			// .attr('stroke-opacity', d => this.scale(d[1]))
+			// .attr('d', d1)
+			.attr('class', 'faf')
+			.attr("d", d => {
+				function gs(d, s) {
+
+					let square = d[0].split('-')[s].toLowerCase();
+
+					let file = square.charCodeAt(0) - 97;
+					let rank = 8 - square[1];
+
+
+					let x = (file * self._options.squareWidth) + (self._options.squareWidth / 2);
+					let y = (rank * self._options.squareWidth) + (self._options.squareWidth / 2);
+
+					return {x,y};
+				}
+
+				let s = gs(d, 0);
+				let e = gs(d, 1);
+
+				let orth = {
+					x: -(e.y - s.y),
+					y: e.x - s.x
+				};
+
+				let norm = Math.sqrt(Math.pow(orth.x, 2) + Math.pow(orth.y, 2));
+				let n1 = Math.sqrt(Math.pow(e.x-s.x, 2) + Math.pow(e.y-s.y, 2)) / 2;
+
+				orth.x /= norm;
+				orth.y /= norm;
+
+				orth.x *= n1;
+				orth.y *= n1;
+
+				let ctrp;
+
+				if( e.x > s.x ) {
+					ctrp = {
+						x: e.x - orth.x,
+						y: e.y - orth.y
+					};
+				} else {
+					ctrp = {
+						x: e.x + orth.x,
+						y: e.y + orth.y
+					};
+				}
+
+				let dv = 7;
+				let de = 2
+				s.x += _.random(-de, de, true)
+				s.y += _.random(-de, de, true)
+				e.x += _.random(-de, de, true)
+				e.y += _.random(-de, de, true)
+				ctrp.x += _.random(-dv, dv, true)
+				ctrp.y += _.random(-dv, dv, true)
+
+				let str = `M${s.x},${s.y}, Q${ctrp.x},${ctrp.y} ${e.x},${e.y}`
+				// log(d)
+				// log(s, e, str)
+				return str;
+			});
+
+		// this.dataContainer.append('path')
+		// .attr('stroke', 'red')
+		// .attr('fill', 'transparent')
+		// .attr('stroke-width', '10')
+		// .attr('d', 'M100,300 Q500,150 500,300')
 	}
 }
