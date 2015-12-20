01e180026c814afc465209841b1b5480794f33b2
ebemunk <ebemunk@gmail.com>
2015-12-20 02:54:15 -0800

    performance

--- scripts/stats.js
+++ scripts/stats.js
@@ -30,41 +30,69 @@ console.log('  source file'.cyan, argv.file);
 
 let heatmaps = new Heatmaps();
 let openings = new Openings();
-let movesz = new Moves();
+let moves = new Moves();
 
 let start = new Date();
+let numGames = 0;
 
 debug(argv);
 
+//precompile regexes for performance
+//not sure if this makes that much difference, but seems to help
+const splitBy = /\r?\n\r?\n(?=\[)/g;
+const r1 = /\[SetUp|FEN\]/;
+const r2 = /^(\[(.|\r?\n)*\])(\r?\n)*1.(\r?\n|.)*$/g;
+const r3 = /\r?\n/g;
+const r4 = /(\{[^}]+\})+?/g;
+const r5 = /\d+\./g;
+const r6 = /\.\.\./g;
+const r7 = /\*/g;
+const r8 = /(1\-0)?(0\-1)?(1\/2\-1\/2)?/g;
+const r9 = /\s+/;
+const r10 = /,,+/g;
+
 highland(fs.createReadStream(argv.file, {encoding: 'utf8'}))
-.splitBy(/\r?\n\r?\n(?=\[)/g) //split pgns with multiple games by game
+.splitBy(splitBy) //split pgns with multiple games by game
 .map(file => {
+	//dont bother with Set-up games
+	if( r1.test(file) ) {
+		return;
+	}
+
 	//regexes stolen from chess.js
-	let moves = file
-	.replace(file.replace(/^(\[(.|\r?\n)*\])(\r?\n)*1.(\r?\n|.)*$/g, '$1'), '') //strip away header text
-	.replace(/\r?\n/g, ' ') //join multiple lines
-	.replace(/(\{[^}]+\})+?/g, '') //remove comments
-	.replace(/\d+\./g, '') //remove move numbers
-	.replace(/\.\.\./g, '') //remove ...
-	.replace(/(1\-0)?(0\-1)?(1\/2\-1\/2)?/g, '') //remove results
+	let gameMoves = file
+	.replace(file.replace(r2, '$1'), '') //strip away header text
+	.replace(r3, ' ') //join multiple lines
+	.replace(r4, '') //remove comments
+	.replace(r5, '') //remove move numbers
+	.replace(r6, '') //remove ...
+	.replace(r7, '') //remove *
+	.replace(r8, '') //remove results
 	.trim()
-	.split(/\s+/) //split by space
+	.split(r9) //split by space
 	//get rid of empty moves
 	.join(',')
-	.replace(/,,+/g, ',')
+	.replace(r10, ',')
 	.split(',');
 
-	heatmaps.update(moves);
-	openings.update(_.take(moves, 10));
-	movesz.update(moves);
+	if( gameMoves.length < 2 ) {
+		//ignore super short games
+		return;
+	}
+
+	heatmaps.update(gameMoves);
+	openings.update(_.take(gameMoves, 7));
+	moves.update(gameMoves);
+	numGames++;
 })
 .done(() => {
-	fs.writeFileAsync(argv.file.split('.pgn')[0] + '_statistics.json', JSON.stringify({
+	fs.writeFileAsync(argv.file.split('.pgn')[0] + '_stats.json', JSON.stringify({
 		heatmaps: heatmaps.data,
 		openings: openings.data,
-		moves: movesz.survivalData
+		moves: moves.survivalData
 	}, null, 4))
 	.then(() => {
 		console.log('  done, took'.cyan, new Date().getTime() - start.getTime(), 'ms'.cyan);
+		console.log('  processed games:', numGames);
 	});
 });
