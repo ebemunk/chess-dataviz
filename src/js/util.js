8fe3eb8b887fdb21011c14a6dec842e11720ee70
ebemunk <ebemunk@gmail.com>
2015-12-20 01:14:20 -0800

    drawBoard function refactor in utils

--- src/js/util.js
+++ src/js/util.js
@@ -56,3 +56,60 @@ export function boardSquares() {
 export function isWhite(d) {
 	return (! (d.x % 2) && ! (d.y % 2)) || (d.x % 2 && d.y % 2);
 }
+
+export function drawBoard(svg, squareWidth) {
+	//board squares
+	let board = boardSquares();
+
+	//create the g elements for squares
+	let squares = svg.selectAll('.square').data(board).enter()
+		.append('g')
+			.attr('class', (d) => {
+				let file = String.fromCharCode(97 + d.x);
+				let rank = 8 - d.y;
+
+				return 'square ' + file + rank;
+			})
+			.classed('white', (d) => isWhite(d))
+			.classed('black', (d) => ! isWhite(d))
+	;
+
+	//create square elements for board squares
+	squares.append('rect')
+		.attr('x', (d) => d.x * squareWidth)
+		.attr('y', (d) => d.y * squareWidth)
+		.attr('width', squareWidth + 'px')
+		.attr('height', squareWidth + 'px')
+		.attr('class', 'sq')
+	;
+
+	//labels among the A file
+	let fileLabels = d3.range(8).map((i) => '.a' + (i + 1));
+
+	//file labels
+	svg.selectAll(fileLabels)
+		.append('text')
+			.attr('x', (d) => d.x * squareWidth)
+			.attr('y', (d) => d.y * squareWidth)
+			.attr('dx', '0.2em')
+			.attr('dy', '1em')
+			.text((d) => 8 - d.y)
+			.attr('class', 'label')
+	;
+
+	//a-h labels for files
+	let files = d3.range(8).map((i) => String.fromCharCode(97 + i));
+	let rankLabels = files.slice().map((file) => '.' + file + '1');
+
+	//rank labels
+	svg.selectAll(rankLabels)
+		.append('text')
+			.attr('x', (d) => (d.x + 1) * .squareWidth)
+			.attr('y', (d) => (d.y + 1) * .squareWidth)
+			.attr('dx', '-0.3em')
+			.attr('dy', '-0.5em')
+			.attr('text-anchor', 'end')
+			.text((d) => files[d.x])
+			.attr('class', 'label')
+	;
+}
