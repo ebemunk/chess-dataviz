7449e881525b74e3d3e205aef89faeff007817db
ebemunk <ebemunk@gmail.com>
2015-12-20 02:53:44 -0800

    movePaths refactor

--- src/js/MovePaths.js
+++ src/js/MovePaths.js
@@ -9,10 +9,6 @@ let log = debug('cv:MovePaths');
 
 export class MovePaths {
 	constructor(selector, options, data) {
-		let self = this;
-
-		if( ! selector ) throw Error('need dom selector');
-
 		//container setup
 		this.container = d3.select(selector);
 
@@ -23,7 +19,8 @@ export class MovePaths {
 			accessor: 'Nb1',
 			binSize: 1,
 			pointRandomizer: d3.random.normal(3, 1),
-			bezierRandomizer: d3.random.normal(12, 4)
+			bezierRandomizer: d3.random.normal(12, 4),
+			bezierScaleFactor: 2
 		};
 
 		options = options || {};
@@ -31,11 +28,6 @@ export class MovePaths {
 
 		this._options.boardWidth = this._options.width - this._options.margin * 2;
 		this._options.squareWidth = Math.floor(this._options.boardWidth / 8);
-
-		//scale
-		this.scale = d3.scale.linear()
-			.range([0, this._options.squareWidth])
-		;
 
 		//clear element
 		this.container.selectAll('*').remove();
@@ -53,60 +45,7 @@ export class MovePaths {
 			.attr('class', 'board')
 		;
 
-		//board squares
-		let board = util.boardSquares();
-
-		//create the g elements for squares
-		let squares = svg.selectAll('.square').data(board).enter()
-			.append('g')
-				.attr('class', (d) => {
-					let file = String.fromCharCode(97 + d.x);
-					let rank = 8 - d.y;
-
-					return 'square ' + file + rank;
-				})
-				.classed('white', (d) => util.isWhite(d))
-				.classed('black', (d) => ! util.isWhite(d))
-		;
-
-		//create square elements for board squares
-		squares.append('rect')
-			.attr('x', (d) => d.x * this._options.squareWidth)
-			.attr('y', (d) => d.y * this._options.squareWidth)
-			.attr('width', this._options.squareWidth + 'px')
-			.attr('height', this._options.squareWidth + 'px')
-			.attr('class', 'sq')
-		;
-
-		//labels among the A file
-		let fileLabels = d3.range(8).map((i) => '.a' + (i + 1));
-
-		//file labels
-		svg.selectAll(fileLabels)
-			.append('text')
-				.attr('x', (d) => d.x * this._options.squareWidth)
-				.attr('y', (d) => d.y * this._options.squareWidth)
-				.attr('dx', '0.2em')
-				.attr('dy', '1em')
-				.text((d) => 8 - d.y)
-				.attr('class', 'label')
-		;
-
-		//a-h labels for files
-		let files = d3.range(8).map((i) => String.fromCharCode(97 + i));
-		let rankLabels = files.slice().map((file) => '.' + file + '1');
-
-		//rank labels
-		svg.selectAll(rankLabels)
-			.append('text')
-				.attr('x', (d) => (d.x + 1) * this._options.squareWidth)
-				.attr('y', (d) => (d.y + 1) * this._options.squareWidth)
-				.attr('dx', '-0.3em')
-				.attr('dy', '-0.5em')
-				.attr('text-anchor', 'end')
-				.text((d) => files[d.x])
-				.attr('class', 'label')
-		;
+		util.drawBoard(svg, this._options.squareWidth);
 
 		//container for heatmap data
 		this.dataContainer = svg.append('g')
@@ -114,9 +53,7 @@ export class MovePaths {
 		;
 
 		if( data ) {
-			this._data = data;
-
-			this.update();
+			this.data(data);
 		}
 	}
 
@@ -127,7 +64,14 @@ export class MovePaths {
 	}
 
 	options(options) {
-		_.merge(this._options, options);
+		let omit = [
+			'width',
+			'margin',
+			'boardWidth',
+			'squareWidth'
+		];
+
+		_.merge(this._options, _.omit(options, omit));
 
 		this.update();
 	}
@@ -163,7 +107,7 @@ export class MovePaths {
 				//get norm (magnitude) of orthogonal
 				let norm = Math.sqrt(Math.pow(orthogonal.x, 2) + Math.pow(orthogonal.y, 2));
 				//scale factor to determine distance of control point from the end point
-				let scaleFactor = Math.sqrt(Math.pow(e.x-s.x, 2) + Math.pow(e.y-s.y, 2)) / 2;
+				let scaleFactor = Math.sqrt(Math.pow(e.x-s.x, 2) + Math.pow(e.y-s.y, 2)) / this._options.bezierScaleFactor;
 
 				//transform the orthogonal vector
 				orthogonal.x /= norm;
